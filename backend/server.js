bda4f1bf03ca20c4217e0ceffc24f2280f720832
Vidhyabisen <122996747+Vidhyabisen@users.noreply.github.com>
2026-06-22 17:02:33 +0530

    frontend url paste kiya hai

--- backend/server.js
+++ backend/server.js
@@ -53,7 +53,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://effortless-trifle-ac9f9a.netlify.app",
+  ],
+  credentials: true,
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
